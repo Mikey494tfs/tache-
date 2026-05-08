d70e1bb16df80219f2fd9cba0ae82c8915f44a4b
Michael Chen <michaelchen@Michaels-MacBook-Air.local>
2026-05-07 23:18:38 -0400

    Initial commit: Project setup and dependencies

---
+++ src/main/java/com/michael/tachemike/TachemikeApplication.java
@@ -0,0 +1,13 @@
+package com.michael.tachemike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TachemikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TachemikeApplication.class, args);
+    }
+
+}
